00f4a89e08bcac6bd0abf64420d1a9f594bc60ef
Mfawzy9 <mahmoud.fawzy.992000@gmail.com>
2025-05-19 19:17:37 +0300

    test

--- src/app/_Components/Home/HomeComp/HomeClient.tsx
+++ src/app/_Components/Home/HomeComp/HomeClient.tsx
@@ -1,59 +1,96 @@
 "use client";
 import PageSection from "@/app/_Components/PageSection/PageSection";
-import LazyRender from "@/app/_Components/LazyRender/LazyRender";
-// import LazyWatchlist from "@/app/_Components/Home/LazySections/LazyWatchlist";
 import dynamic from "next/dynamic";
 
-// const ShortDetailsSkeleton = dynamic(
-//   () => import("@/app/_Components/ShortDetails/ShortDetailsSkeleton"),
-// );
-// const GenresSkeletonSlider = dynamic(
-//   () => import("@/app/_Components/GenresSlider/GenresSkeletonSlider"),
-// );
-// const VideosSkelsetonSlider = dynamic(
-//   () => import("@/app/_Components/VideosSlider/VideosSkelsetonSlider"),
-// );
+const LazyRender = dynamic(
+  () => import("@/app/_Components/LazyRender/LazyRender"),
+  {
+    ssr: false,
+  },
+);
+
+const ShortDetailsSkeleton = dynamic(
+  () => import("@/app/_Components/ShortDetails/ShortDetailsSkeleton"),
+  {
+    ssr: false,
+  },
+);
+
+const GenresSkeletonSlider = dynamic(
+  () => import("@/app/_Components/GenresSlider/GenresSkeletonSlider"),
+  {
+    ssr: false,
+  },
+);
+
+const VideosSkelsetonSlider = dynamic(
+  () => import("@/app/_Components/VideosSlider/VideosSkelsetonSlider"),
+  {
+    ssr: false,
+  },
+);
+
 const HoriSkeletonSlider = dynamic(
   () => import("@/app/_Components/HoriCardsSlider/HoriSkeletonSlider"),
+  {
+    ssr: false,
+  },
 );
-// const CardsSkeletonSlider = dynamic(
-//   () => import("@/app/_Components/CardsSlider/CardsSkeletonSlider"),
-// );
-// const LazyTopRatedMovies = dynamic(
-//   () => import("@/app/_Components/Home/LazySections/LazyTopRatedMovies"),
-//   {
-//     ssr: false,
-//     loading: () => <HoriSkeletonSlider />,
-//   },
-// );
-// const LazyLastWatched = dynamic(
-//   () => import("@/app/_Components/Home/WatchHistory/LastWatched"),
-//   {
-//     ssr: false,
-//     loading: () => <ShortDetailsSkeleton />,
-//   },
-// );
-// const LazyTrendingTvShows = dynamic(
-//   () => import("@/app/_Components/Home/LazySections/LazyTrendingTvShows"),
-//   {
-//     ssr: false,
-//     loading: () => <CardsSkeletonSlider />,
-//   },
-// );
-// const LazyLatestTrailers = dynamic(
-//   () => import("@/app/_Components/Home/LazySections/LazyLatestTrailers"),
-//   {
-//     ssr: false,
-//     loading: () => <VideosSkelsetonSlider />,
-//   },
-// );
-// const LazyUpcomingMovies = dynamic(
-//   () => import("@/app/_Components/Home/LazySections/LazyUpcomingMovies"),
-//   {
-//     ssr: false,
-//     loading: () => <CardsSkeletonSlider />,
-//   },
-// );
+
+const CardsSkeletonSlider = dynamic(
+  () => import("@/app/_Components/CardsSlider/CardsSkeletonSlider"),
+  {
+    ssr: false,
+  },
+);
+
+const LazyTopRatedMovies = dynamic(
+  () => import("@/app/_Components/Home/LazySections/LazyTopRatedMovies"),
+  {
+    ssr: false,
+    loading: () => <HoriSkeletonSlider />,
+  },
+);
+
+const LazyLastWatched = dynamic(
+  () => import("@/app/_Components/Home/WatchHistory/LastWatched"),
+  {
+    ssr: false,
+    loading: () => <ShortDetailsSkeleton />,
+  },
+);
+
+const LazyTrendingTvShows = dynamic(
+  () => import("@/app/_Components/Home/LazySections/LazyTrendingTvShows"),
+  {
+    ssr: false,
+    loading: () => <CardsSkeletonSlider />,
+  },
+);
+
+const LazyWatchlist = dynamic(
+  () => import("@/app/_Components/Home/LazySections/LazyWatchlist"),
+  {
+    ssr: false,
+  },
+);
+
+const LazyLatestTrailers = dynamic(
+  () => import("@/app/_Components/Home/LazySections/LazyLatestTrailers"),
+  {
+    ssr: false,
+    loading: () => <VideosSkelsetonSlider />,
+  },
+);
+
+const LazyUpcomingMovies = dynamic(
+  () => import("@/app/_Components/Home/LazySections/LazyUpcomingMovies"),
+  {
+    ssr: false,
+    loading: () => <CardsSkeletonSlider />,
+  },
+);
+
 const LazyMostVotedTvShows = dynamic(
   () => import("@/app/_Components/Home/LazySections/LazyMostVotedTvShows"),
   {
@@ -61,56 +98,57 @@ const LazyMostVotedTvShows = dynamic(
     loading: () => <HoriSkeletonSlider />,
   },
 );
-// const LazyGenres = dynamic(
-//   () => import("@/app/_Components/Home/LazySections/LazyGenres"),
-//   {
-//     ssr: false,
-//     loading: () => <GenresSkeletonSlider />,
-//   },
-// );
+
+const LazyGenres = dynamic(
+  () => import("@/app/_Components/Home/LazySections/LazyGenres"),
+  {
+    ssr: false,
+    loading: () => <GenresSkeletonSlider />,
+  },
+);
 
 const HomeClient = () => {
   return (
     <>
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* top rated movies */}
-      {/* <LazyRender
+      <LazyRender
         Component={LazyTopRatedMovies}
         loading={<HoriSkeletonSlider />}
-      /> */}
+      />
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* last watched show */}
-      {/* <LazyRender
+      <LazyRender
         Component={LazyLastWatched}
         loading={<ShortDetailsSkeleton className="!py-0 mt-14" />}
-      /> */}
+      />
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* trending today tv shows */}
-      {/* <PageSection>
+      <PageSection>
         <LazyRender
           Component={LazyTrendingTvShows}
           loading={<CardsSkeletonSlider />}
         />
-      </PageSection> */}
+      </PageSection>
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* Watchlist */}
-      {/* <PageSection className="!py-5">
+      <PageSection className="!py-5">
         <LazyWatchlist />
-      </PageSection> */}
+      </PageSection>
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* latest trailers */}
-      {/* <LazyRender
+      <LazyRender
         Component={LazyLatestTrailers}
         loading={<VideosSkelsetonSlider />}
-      /> */}
+      />
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* Upcoming Movies */}
-      {/* <PageSection className="!py-5">
+      <PageSection className="!py-5">
         <LazyRender
           Component={LazyUpcomingMovies}
           loading={<CardsSkeletonSlider />}
         />
-      </PageSection> */}
+      </PageSection>
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* most rated Tv Shows */}
       <LazyRender
@@ -119,7 +157,7 @@ const HomeClient = () => {
       />
       {/* ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- */}
       {/* genres Section */}
-      {/* <LazyRender Component={LazyGenres} loading={<GenresSkeletonSlider />} /> */}
+      <LazyRender Component={LazyGenres} loading={<GenresSkeletonSlider />} />
     </>
   );
 };
--- src/app/_Components/Home/HomeComp/HomeServer.tsx
+++ src/app/_Components/Home/HomeComp/HomeServer.tsx
@@ -1,28 +1,28 @@
-// import dynamic from "next/dynamic";
-// import HomeSliderFetcher from "../../MainHomeSlider/HomeSliderFetcher";
+import dynamic from "next/dynamic";
+import HomeSliderFetcher from "../../MainHomeSlider/HomeSliderFetcher";
 import HomeClient from "./HomeClient";
 
-// const WatchHistory = dynamic(
-//   () => import("@/app/_Components/Home/WatchHistory/WatchHistory"),
-// );
-// const PopularPplHomeSlider = dynamic(
-//   () => import("@/app/_Components/Home/PopularPplHomeSlider"),
-//   {
-//     loading: () => {
-//       const CardsSkeletonSlider = dynamic(
-//         () => import("@/app/_Components/CardsSlider/CardsSkeletonSlider"),
-//       );
-//       return <CardsSkeletonSlider />;
-//     },
-//   },
-// );
+const WatchHistory = dynamic(
+  () => import("@/app/_Components/Home/WatchHistory/WatchHistory"),
+);
+const PopularPplHomeSlider = dynamic(
+  () => import("@/app/_Components/Home/PopularPplHomeSlider"),
+  {
+    loading: () => {
+      const CardsSkeletonSlider = dynamic(
+        () => import("@/app/_Components/CardsSlider/CardsSkeletonSlider"),
+      );
+      return <CardsSkeletonSlider />;
+    },
+  },
+);
 
 const HomeServer = () => {
   return (
     <>
-      {/* <HomeSliderFetcher /> */}
-      {/* <WatchHistory /> */}
-      {/* <PopularPplHomeSlider /> */}
+      <HomeSliderFetcher />
+      <WatchHistory />
+      <PopularPplHomeSlider />
       <HomeClient />
     </>
   );
