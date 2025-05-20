a333d8f461f80f4c68dae3d2f7fb329793216559
Mfawzy9 <mahmoud.fawzy.992000@gmail.com>
2025-05-20 13:15:31 +0300

    test Minified React error

--- src/app/hooks/useOnlineStatus.ts
+++ src/app/hooks/useOnlineStatus.ts
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 export const useOnlineStatus = () => {
   const [isOnline, setIsOnline] = useState(true);
   useEffect(() => {
+    if (typeof window === "undefined") return;
     const updateStatus = () => setIsOnline(navigator.onLine);
     window.addEventListener("online", updateStatus);
     window.addEventListener("offline", updateStatus);
